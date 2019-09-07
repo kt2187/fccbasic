01d740b905ce590b9ca091ad6a29ce44c5cfd56c
Katie Barriere <kbarriere@gmail.com>
2019-09-06 21:25:11 -0400

    confirm-ending

---
+++ confirm-ending.js
@@ -0,0 +1,17 @@
+// Basic Algorithm Scripting: Confirm the Ending
+// Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+// This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+function confirmEnding(str, target) {
+    return str.substr(str.length - target.length) === target;
+}
+
+//test
+confirmEnding("Bastian", "n");
+
+  //Research
+  //https://www.dyn-web.com/javascript/strings/slice.php
+  //https://love2dev.com/blog/javascript-substring-substr-slice/
