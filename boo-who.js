5c70caf56000b15187806c40ad7fc55033e44042
Katie Barriere <kbarriere@gmail.com>
2019-09-07 20:05:47 -0400

    boo-who

---
+++ boo-who.js
@@ -0,0 +1,16 @@
+// Basic Algorithm Scripting: Boo who
+// Check if a value is classified as a boolean primitive. Return true or false.
+
+// Boolean primitives are true and false.
+
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+
+function booWho(bool) {
+    if (bool === true || bool === false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(booWho(null));
