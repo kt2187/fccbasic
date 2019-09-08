c251d3022cdcc97926793026258cd877063732e2
Katie Barriere <kbarriere@gmail.com>
2019-09-07 20:39:20 -0400

    title-case

---
+++ title-case.js
@@ -0,0 +1,16 @@
+// Basic Algorithm Scripting: Title Case a Sentence
+// Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+// For the purpose of this exercise, you should also capitalize connecting words like "the" and "of".
+
+// Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+function titleCase(str) {
+    var array = str.split(" ");
+    var newArray = array.map(function(word) {
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+    return newArray.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
